e38315116e870197859a34b922e809abdc514003
wuliuxin <13545064261@163.com>
2018-11-06 21:29:09 +0800

    发表完成，完成文章详情路由跳转

--- router/user.js
+++ router/user.js
@@ -1,21 +1,16 @@
-const express =require('express')
-const router =express.Router()
-// // mysql.createConnection()node链接数据库的方法
-// const conn =mysql.createConnection({
-//     host:'127.0.0.1',
-//     user:'root',
-//     password:'root',
-//     database:'my_blog'
-// })
-// 导入 用户相关的 处理函数模块
-const ctrl = require('../controller/user.js')
-// 监听客户端的请求 把客户端这个传输给服务器
-router.get('/register',ctrl.showRegisterPage);
-router.get('/login',ctrl.showLoginPage);
-// 注册逻辑
-router.post('/register',ctrl.reg);
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/user.js')
 
-router.post('/login',ctrl.login)
+// 分发路由
+router.get('/register', controller.handleRegisterGet);
 
-module.exports=router
-// 导出路由
+router.get('/login', controller.handleLoginGet);
+
+router.post('/register', controller.handleRegisterPost);
+
+router.post('/login', controller.handleLoginPost);
+
+router.get('/logout', controller.handleLogoutGet);
+
+module.exports = router
